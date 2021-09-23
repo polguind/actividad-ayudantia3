4eb658dc0d58bda222a9e8487105300e9f21c75f
Paola Olguín <pfolguin@uc.cl>
2021-09-23 18:23:31 -0300

    lista a4

---
+++ enchule.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+     var  todo =$(".todo");
+    todo.mouseenter(function(){
+        console.log("hola")
+        $("#color").text("Estas fueron mis lecturas de agosto")
+        $("#color").css("color","blue")
+     });
+
+    todo.mouseleave(function(){
+        $("#color").text("Estas fueron mis lecturas de agosto")
+        $("#color").css("color","blueviolet")
+     });
+   
+    todo.click(function(){
+        $("#texto").text()
+        $("#texto").css("color","blue")
+    });
+
+});
+
+
+$(document).ready(function(){
+    var  todito =$(".todito");
+    todito.click(function(){
+        $("#escondido").text()
+        $('#escondido').text('El primer día de cada mes lanzo el wrap up del mes anterior ¡Atentx!')
+    });
+    
+});
